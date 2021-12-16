55900046c2080063e376cd73d59154bfbcd91d96
Luca Arce <luca.a127@gmail.com>
2021-12-16 18:41:28 -0300

    probando ramas

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     
     <div className="App">
-      {/* <Test /> */}
+      <Test />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
