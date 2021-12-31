86c334e24dbe5276da29f0ab077fa40b6c728452
Luca Arce <luca.a127@gmail.com>
2021-12-30 23:58:12 -0300

    clase_6

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-import react from 'react';
+// import react from 'react';
 import NavBarBS from './component/NavBar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import ItemListContainer from './component/ItemListContainer';
-import ItemCount from './component/ItemCount';
+// import ItemCount from './component/ItemCount';
 
 
 
@@ -17,7 +17,7 @@ function App() {
       
       <ItemListContainer greeting='Bienvenido a nuestra tienda!' />
 
-      <ItemCount minimo={1} maximo={10} />
+      {/* <ItemCount minimo={1} maximo={10} /> */}
 
 
 
--- src/component/ItemListContainer.js
+++ src/component/ItemListContainer.js
@@ -1,11 +1,54 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
+import { getFetch } from '../helper/mock'
 
-const ItemListContainer = (props) => {
+
+
+
+function ItemListContainer({greeting}) {
+    const [productos, setProductos] = useState([])
+
+    useEffect(() => {
+        getFetch
+        .then(resp => setProductos(resp))
+        .catch(err => console.log(err))
+        .finally(()=> console.log('Final') ) 
+    }, [])
+
+
+
+        console.log(productos)
+
     return (
-        <div>
-            <h1>{props.greeting}</h1>
-        </div>
+      <div>
+          <h4>{greeting}</h4>
+          {productos.map(prod => 
+            <div> 
+              <li>{prod.id}</li> 
+              <li>{prod.detalle}</li>
+              
+            </div>
+
+          
+          )}
+      </div>
     )
+
 }
 
+
 export default ItemListContainer
+
+
+
+
+
+
+// const ItemListContainer = (props) => {
+//     return (
+//         <div>
+//             <h1>{props.greeting}</h1>
+//         </div>
+//     )
+// }
+
+// export default ItemListContainer
